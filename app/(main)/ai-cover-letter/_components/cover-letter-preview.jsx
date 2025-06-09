78b1920346589662f4acfb6e99af3fe96661bd70
MANImax123 <lenkalapellymaniteja142@gmail.com>
2025-06-09 19:54:16 +0530

    changes

---
+++ app/(main)/ai-cover-letter/_components/cover-letter-preview.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import MDEditor from "@uiw/react-md-editor";
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
+
+const CoverLetterPreview = ({ content }) => {
+  return (
+    <div className="py-4">
+      <MDEditor value={content} preview="preview" height={700} />
+    </div>
+  );
+};
+
+export default CoverLetterPreview;
